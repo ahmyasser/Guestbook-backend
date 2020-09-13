8b4d6bfbe139c1279484c4fc5445b2a9ea0d4683
Ahmed Yasser <ahmedtricker@gmial.com>
2020-09-13 15:19:08 +0200

    mongo set up done

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
-module.exports = {   uri: "mongodb+srv://traildb19912:WzfeODgtvhz9HOE0@cluster0.pynpu.mongodb.net/note-api?retryWrites=true&w=majority"};
+module.exports = {  MONGOURI: "mongodb+srv://traildb19912:WzfeODgtvhz9HOE0@cluster0.pynpu.mongodb.net/note-api?retryWrites=true&w=majority"};
 
 
