d5c49747bba54c58e0aa0aa0f100736f71bc1d66
Ahmed Yasser <ahmedtricker@gmial.com>
2020-09-13 23:46:40 +0200

    signup and signin are  done

--- app.js
+++ app.js
@@ -15,3 +15,15 @@ mongoose.connection.on('connected',()=>{
 mongoose.connection.on('error',(err)=>{
     console.log("err connecting",err)
 })
+
+require ('./models/user');
+
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
