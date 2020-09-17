da32f1bdeb0e17ff5740f45c9055a0a76182d5e3
Ahmed Yasser <ahmedtricker@gmial.com>
2020-09-17 02:57:02 +0200

    done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 8000
-const {MONGOURI} = require('./config/db')
+const {MONGOURI} = require('./config/keys')
 
 mongoose.connect(MONGOURI,{
     useNewUrlParser:true,
@@ -25,8 +25,16 @@ app.use(express.json())
 app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 
 app.listen(PORT,()=>{
+
     console.log("server is running on",PORT)
 })
